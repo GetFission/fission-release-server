c898029a07d9dc793f52ee365758e0debbe9fa4f
simplyahmazing <ahmad19526@gmail.com>
2017-09-15 10:12:29 -0400

    setup typescript

--- npm-package/dist/index.js
+++ npm-package/dist/index.js
@@ -1,5 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var FissionUpdater = (function () {
+    function FissionUpdater(id) {
+        this.clientId = id;
+    }
+    return FissionUpdater;
+}());
+// export const new FissionUpdater() : Updater
 function print(msg) {
     console.log("[Printing]", msg);
 }
--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -1,3 +1,18 @@
+import { EventEmitter } from "events"
+
+class FissionUpdater {
+  clientId : string
+
+  constructor(id : string) {
+    this.clientId = id
+  }
+}
+
+
+// export const new FissionUpdater() : Updater
+
+
+
 export function print(msg : String) {
   console.log("[Printing]", msg)
 }
