7b2f5123e1bf7c09076ebe5de7b66afc3cfcf46d
simplyahmazing <ahmad19526@gmail.com>
2017-10-13 19:55:42 -0400

    refactoring project base

--- server/src/manage.py
+++ server/src/manage.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+import dotenv
 import os
 import sys
+from configurations import importer
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoreactredux.settings.dev")
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
