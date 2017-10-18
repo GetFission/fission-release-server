b069b89da89dcd18ef5bcc63e80342d862d19840
Lev Kravinsky <kravinskylev@gmail.com>
2017-10-18 14:18:11 -0400

    fix webpack issues with static asset loading, fix typo in pg user init

--- server/tools/init-user-db.sh
+++ server/tools/init-user-db.sh
@@ -4,6 +4,6 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER electronfission WITH PASSWORD 'password' CREATEDB;
-    CREATE DATABASE electronfissiondev_dev;
+    CREATE DATABASE electronfission_dev;
     GRANT ALL PRIVILEGES ON DATABASE electronfission_dev TO electronfission;
 EOSQL
