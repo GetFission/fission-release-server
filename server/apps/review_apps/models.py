0126186f983d7a8157c85c00e40e9fa8a929c581
simplyahmazing <ahmad19526@gmail.com>
2017-10-24 12:49:58 -0400

    validate api_key before saving

--- server/apps/review_apps/models.py
+++ server/apps/review_apps/models.py
@@ -1,8 +1,13 @@
+import uuid
+
 from django.db import models
 from django_extensions.db import models as dj_models
 
 
 class ReviewAppBuild(dj_models.TimeStampedModel):
+    project = models.ForeignKey('projects.Project', null=True)
+    api_key = models.UUIDField(default=uuid.uuid4, blank=True, null=True)
+
     PLATFORM_CHOICES = (
         ('darwin', 'darwin'),
         ('linux32', 'linux32'),
@@ -10,16 +15,14 @@ class ReviewAppBuild(dj_models.TimeStampedModel):
         ('win32', 'win32'),
         ('win64', 'win64')
     )
+    platform = models.CharField(max_length=10, choices=PLATFORM_CHOICES)
+
     CI_CHOICES = (
         ('appveyor', 'appveyor'),
         ('travis', 'travis'),
         ('teamcity', 'teamcity'),
         ('jenkins', 'jenkins'),
     )
-
-    # project = models.ForeignKey('projects.project')
-
-    platform = models.CharField(max_length=10, choices=PLATFORM_CHOICES)
     ci = models.CharField(max_length=50, choices=CI_CHOICES)
 
     app_version = models.CharField(max_length=50, blank=True, null=True)
--- server/apps/review_apps/serializers.py
+++ server/apps/review_apps/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
+from django.core.exceptions import ValidationError
 
+from projects import models as project_models
 from review_apps import models
 
 
+def validate_api_key(api_key):
+    try:
+        if project_models.Project.objects.filter(api_key=api_key).count() == 1:
+            return api_key
+    except ValidationError:
+        raise serializers.ValidationError('Improperly formatted API KEY format')
+    except Exception as e:
+        print('Failed to find api key', e)
+        raise serializers.ValidationError('Invalid API KEY')
+
+
 class ReviewAppBuildSerializer(serializers.ModelSerializer):
+    api_key = serializers.CharField(max_length=255,  validators=[validate_api_key])
     class Meta:
         model = models.ReviewAppBuild
         fields = (
             'id',
+            'project',
+            'api_key',
             'platform',
             'ci',
             'app_version',
@@ -17,5 +33,12 @@ class ReviewAppBuildSerializer(serializers.ModelSerializer):
             'pull_request_number'
         )
 
-    # def validate_api_key(self):
-    #     pass
+    extra_kwargs = {"api_key": {"error_messages": {"required": "Valid api_key required"}}}
+
+    def create(self, validated_data):
+        api_key = validated_data.pop('api_key')
+        project = project_models.Project.objects.get(api_key=api_key)
+        validated_data['project'] = project
+        build = models.ReviewAppBuild.objects.create(**validated_data)
+        return build
+
