451c0424ea31cf945cb9a9db5af5225e1886a7a7
simplyahmazing <ahmad19526@gmail.com>
2017-11-02 13:13:30 -0400

    refactor data model

--- apps/review_apps/models.py
+++ apps/review_apps/models.py
@@ -2,8 +2,37 @@ from django.db import models
 from django_extensions.db import models as dj_models
 
 
+class Branch(dj_models.TimeStampedModel):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    project = models.ForeignKey(
+        'projects.Project',
+        null=True,
+        blank=True,
+        related_name='branches'
+    )
+
+
+class Commit(dj_models.TimeStampedModel):
+    branch = models.ForeignKey(
+        'review_apps.Branch',
+        null=True,
+        blank=True,
+        related_name='commits'
+    )
+    commit_hash = models.CharField(
+        unique=True,
+        max_length=255,
+        blank=True,
+        null=True
+    )
+
+
 class ReviewAppBuild(dj_models.TimeStampedModel):
-    project = models.ForeignKey('projects.Project', null=True)
+    commit = models.ForeignKey(
+        'review_apps.Commit',
+        null=True,
+        related_name='builds'
+    )
     api_key = models.UUIDField(blank=True, null=True)
 
     PLATFORM_CHOICES = (
@@ -26,10 +55,8 @@ class ReviewAppBuild(dj_models.TimeStampedModel):
     ci = models.CharField(max_length=50, choices=CI_CHOICES)
 
     app_version = models.CharField(max_length=50, blank=True, null=True)
-    branch_name = models.CharField(max_length=255, blank=True, null=True)
-    commit_hash = models.CharField(max_length=255, blank=True, null=True)
 
-    build_url = models.CharField(max_length=50, blank=True, null=True)
+    build_url = models.CharField(max_length=255, blank=True, null=True)
     ci_job_id = models.CharField(max_length=50, blank=True, null=True)
     pull_request_number = models.CharField(
         max_length=255, blank=True, null=True
--- apps/review_apps/views.py
+++ apps/review_apps/views.py
@@ -9,15 +9,56 @@ class ReviewAppBuildCreateView(generics.CreateAPIView):
 
 
 class ReviewAppBuildListView(generics.ListAPIView):
-    queryset = models.ReviewAppBuild.objects.all()
-    serializer_class = serializers.ReviewAppBuildListSerializer
+    # queryset = models.ReviewAppBuild.objects.all()
+    # queryset = models.Branch.objects.all()
+    serializer_class = serializers.BranchSerializer
 
     def get_queryset(self):
         project_slug = self.kwargs['project_slug']
-        return models.ReviewAppBuild.objects.filter(project__slug=project_slug)
+        return models.Branch.objects.filter(project__slug=project_slug)
 
 
 class ReviewAppBuildDetail(generics.RetrieveUpdateAPIView):
     queryset = models.ReviewAppBuild.objects.all()
-    serializer_class = serializers.ReviewAppBuildListSerializer
+    serializer_class = serializers.ReviewAppBuildSerializer
+
+
+"""
+/review-apps/<project-slug>/<branch>/
+{'builds': [build 1...N]}
+
+/review-apps/<project-slug>/<get-branches>/
+{'branches': [branch 1....N]}
+
+/review-apps/<project-slug>/?page=2
+{'branches': 'master': [build 1...N], 'hotfix': [build 1.... N]}
+
+/review-apps/<project-slug>/build-id/detail/
+{...build info}
+
+"""
+
+# class CommitsListView(generics.ListAPIView):
+#     # queryset = models.ReviewAppBuild.objects.all()
+#     serializer_class = serializers.ReviewAppBuildListSerializer
+#
+#     def get_queryset(self):
+#         project_slug = self.kwargs['project_slug']
+#         commits = (
+#             models.Commit.objects
+#             .filter(project__slug=project_slug)
+#             .order_by('-created')
+#             .distinct('branch')
+#             .limit(10)
+#         )
+#         branches = [commit.branch for commit in commits]
+#
+#         data = {}
+#         for branch in branches:
+#             data[branch] = models.Commit.objects.filter(
+#                 branch=branch,
+#                 project__slug=project_slug
+#             )
+#         return data
+#
 
