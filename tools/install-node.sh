87131e8a26e95cb6aea3de3a19915ce730cd17b8
simplyahmazing <ahmad19526@gmail.com>
2017-11-21 14:45:53 -0500

    try node install script

---
+++ tools/install-node.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ex
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+nvm install v8.9.0
+echo "Node installed"
