617ac0a6db59683d082edcac0eeea23107ec93b7
simplyahmazing <ahmad19526@gmail.com>
2017-11-21 23:24:39 -0500

    setup nvm for building js code

--- tools/js-build.sh
+++ tools/js-build.sh
@@ -2,12 +2,10 @@
 
 set -e
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 
-nvm install v8.9.1
-echo "Node installed"
+nvm use v8.9.1
 
 npm install
 npm run build
