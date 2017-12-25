24febcdfed620c5d7d993030c790a570b298271f
simplyahmazing <ahmad19526@gmail.com>
2017-12-24 21:41:22 -0500

    edit dashboard page

--- assets/src/components/DashboardSidebar.vue
+++ assets/src/components/DashboardSidebar.vue
@@ -5,61 +5,56 @@
     </p>
     <ul class="menu-list">
       <li>
-        <a class="is-active">Dashboard</a>
-      </li>
-      <li>
-        <a>Customers</a>
+        <a class="is-active">Overview</a>
       </li>
     </ul>
+    <p class="menu-label">
+      Projects <span class="tag is-info">2</span>
+    </p>
+    <ul class="menu-list">
+      <li>
+        <a>Zulip</a>
+        <ul>
+          <li>
+            <a>Dashboard</a>
+          </li>
+          <li>
+            <a>Releases</a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a>My Other App</a>
+        <ul>
+          <li>
+            <a>Dashboard</a>
+          </li>
+          <li>
+            <a>Releases</a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <a class="button">Add project</a>
     <p class="menu-label">
       Administration
     </p>
     <ul class="menu-list">
       <li>
-        <a>Team Settings</a>
+        <a>Account Settings</a>
       </li>
-      <li>
-        <a>Manage Your Team</a>
-        <ul>
-          <li>
-            <a>Members</a>
-          </li>
-          <li>
-            <a>Plugins</a>
-          </li>
-          <li>
-            <a>Add a member</a>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <a>Invitations</a>
-      </li>
-      <li>
-        <a>Cloud Storage Environment Settings</a>
-      </li>
-      <li>
-        <a>Authentication</a>
-      </li>
-    </ul>
-    <p class="menu-label">
-      Transactions
-    </p>
-    <ul class="menu-list">
       <li>
         <a>Payments</a>
       </li>
-      <li>
-        <a>Transfers</a>
-      </li>
-      <li>
-        <a>Balance</a>
-      </li>
     </ul>
   </aside>
-
 </template>
-
 
 <script>
 import Vue from 'vue'
