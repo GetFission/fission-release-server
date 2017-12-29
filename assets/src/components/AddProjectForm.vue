ccecb419fa0c399e5449efb321acb4d27799bb9f
simplyahmazing <ahmad19526@gmail.com>
2017-12-29 14:35:25 -0500

    start project form component

---
+++ assets/src/components/AddProjectForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="add-project-form">
+    coming soon...
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+Vue.component('add-project-form', {
+  data: function () {
+    return {
+    }
+  }
+})
+</script>
+
