c3173e39efca02dd5a39e6ea597cd65de74fa0a7
simplyahmazing <ahmad19526@gmail.com>
2018-01-02 09:35:53 -0500

    fix route

--- assets/src/services/projects.js
+++ assets/src/services/projects.js
@@ -3,7 +3,7 @@ import { http, setHeader } from './http'
 class ProjectsAPI {
   async addProject (data) {
     setHeader('Authorization', `Bearer ${localStorage.getItem('id_token')}`)
-    const path = '/api/project/create/'
+    const path = '/api/v1/projects/create/'
     return http.post(path, data)
   }
 }
