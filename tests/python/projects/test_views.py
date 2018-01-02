bf9ed21d1a7955751d508c54df6f54f9a586f71d
simplyahmazing <ahmad19526@gmail.com>
2018-01-02 17:42:44 -0500

    remove commented line

--- tests/python/projects/test_views.py
+++ tests/python/projects/test_views.py
@@ -1,7 +1,6 @@
 import pytest
 
 from django.contrib.auth.models import User
-# from django.core.urlresolvers import reverse
 
 from rest_framework import status
 from rest_framework.test import APIClient
