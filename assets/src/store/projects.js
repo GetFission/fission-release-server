19d02180a64700bb3f81cf14188f7fcf290a9c00
simplyahmazing <ahmad19526@gmail.com>
2018-01-02 20:10:40 -0500

    update projects view

--- assets/src/store/projects.js
+++ assets/src/store/projects.js
@@ -1,6 +1,3 @@
-// // eslint-disable import/first
-// import { polyfill } from 'es6-promise'
-// polyfill()
 import ProjectsAPI from '../services/projects'
 
 const projectsApi = new ProjectsAPI()
@@ -11,15 +8,6 @@ const user = {
     projects: []
   },
   actions: {
-    // async ADD_PROJECT (context, payload) {
-    //   await projectsApi.addProject(payload)
-    //     .then((data) => {
-    //       console.log('new project added...')
-    //     })
-    //     .catch((err) => {
-    //       console.log('failed to create project')
-    //     })
-    // },
     async LOAD_PROJECTS ({ commit }) {
       await projectsApi.loadProjects()
         .then((data) => {
@@ -32,11 +20,6 @@ const user = {
         })
     }
   },
-  getters: {
-    // TODO: properly implement these
-    // GET_STATE: state => store.state,
-    // GET_ERRORS: state => store.errors
-  },
   mutations: {
     UPDATE_PROJECTS (state, projects) {
       state.projects = projects
@@ -46,10 +29,6 @@ const user = {
     },
     SET_ERROR (state, message = {}) {
       state.error = message
-    },
-    SET_STATE (state, action) {
-      // TODO: revisit use of statemachine to indicate UI loading
-      // state.machine.do(action)
     }
   }
 }
