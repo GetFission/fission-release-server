90744ad2ec31d9bcfe1fa888d09def17d9c41100
simplyahmazing <ahmad19526@gmail.com>
2018-01-08 14:57:41 -0500

    adds test for release creation form

--- tests/python/releases/test_views.py
+++ tests/python/releases/test_views.py
@@ -1,17 +1,29 @@
-import pytest
+import os
 
+import pytest
 from django.contrib.auth.models import User
 
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from projects import models as project_models
+
 from releases import models as releases_models
+
+
+DATA_DIR = os.path.dirname(__file__)
 
 
 @pytest.fixture
 def user():
     user = User.objects.create(username='foo')
     return user
+
+
+@pytest.fixture
+def project():
+    project = project_models.Project.objects.create(name='foo')
+    return project
 
 
 @pytest.fixture
@@ -22,18 +34,29 @@ def client(user):
 
 
 @pytest.mark.django_db
-def test_create_view(client):
+def test_create_view(client, project):
     resp = client.post('/api/v1/releases/create/', {})
     assert resp.status_code == 400
     assert resp.json()['version'] == ["This field is required."]
+    assert resp.json()['project'] == ["This field is required."]
 
-    # resp = client.post('/api/v1/projects/create/', {
-    #     'name': 'Foo Project'
-    # })
-    # assert resp.status_code == 201
-    # expected_json = {
-    #     'name': 'Foo Project', 'rms_url': None, 'slug': 'foo-project'}
-    # assert resp.json() == expected_json
+    resp = client.post('/api/v1/releases/create/', {
+        'name': 'alpha',
+        'version': '1.1.1',
+        'darwin_artifact': open(os.path.join(DATA_DIR, 'data/darwin_test_file.txt')),
+        'windows_artifact': open(os.path.join(DATA_DIR, 'data/windows_test_file.txt')),
+        'project': project.id
+    })
+
+    assert resp.status_code == 201
+
+    expected_json = {'name': 'alpha', 'version': '1.1.1', 'project': 1}
+    res_json = resp.json()
+    darwin_artifact_path = res_json.pop('darwin_artifact')
+    windows_artifact_path = res_json.pop('windows_artifact')
+    assert 'amazonaws.com' in darwin_artifact_path
+    assert 'amazonaws.com' in windows_artifact_path
+    assert res_json == expected_json
 
 
 # @pytest.mark.django_db
