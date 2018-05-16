bbab60cbd794fbb9b2acd81bcc4ccb07ca1afece
simplyahmazing <ahmad19526@gmail.com>
2018-05-15 21:05:09 -0400

    dummy endpoint serves static file

--- apps/updates/views.py
+++ apps/updates/views.py
@@ -5,13 +5,16 @@ def get_update_info(client_info):
     """Given a user and  app update URL, returns
     """
     # Notes: https://github.com/Squirrel/Squirrel.Mac#update-server-json-format
+    # return {
+    #     'url': 'https://gef-test.s3.amazonaws.com/electron-updater-example-0.6.0-mac.zip',
+    #     'version': '0.6.0',
+    #     'pub_date': 123
+    # }
     return {
-        'url': 'https://gef-test.s3.amazonaws.com/electron-updater-example-0.6.0-mac.zip',
-        'version': '0.6.0',
-        'pub_date': 123
-    }
-    return {
-        'files': ['https://gef-test.s3.amazonaws.com/electron-updater-example-0.6.0-mac.zip'],
+        'files': [{
+            'url': 'http://localhost:8005/v6/electron-updater-example-0.6.0-mac.zip',
+            'sha512': 'Sby01m3bYYslcBqqXVD4bE07eM0OjxQDJLnC3o+FJu4m7ohmxmVl65Epq13etYPQzzHJx+pTbJ1uQDCTAImDUA==' }
+        ],
         'version': '0.6.0',
         'releaseDate': '2018-01-16T20:52:11.688Z'
     }
@@ -28,6 +31,7 @@ def get_client_info(request):
     return client_info
 
 def serve_update(request):
+    # import pdb; pdb.set_trace()
     update_info = get_update_info(get_client_info(request))
     return JsonResponse(update_info)
 
